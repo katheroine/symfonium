5079edd3b27617ed7c57cc2bdc7d5954840114d1
katheroine <katheroine@gmail.com>
2023-11-16 18:10:04 +0100

    Post creating page created with dummy post creation action

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,14 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Post;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
+    #[Route('/posts', name: 'index_posts')]
     public function index(): Response
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
         ]);
+    }
+
+    #[Route('/post', name: 'create_post')]
+    public function createPost(EntityManagerInterface $entityManager): Response
+    {
+        $post = new Post();
+        $post->setTitle('Welcome');
+        $post->setContent("Hi, there!");
+        $post->setAuthor('Katheroine');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($post);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new post with id ' . $post->getId());
     }
 }
